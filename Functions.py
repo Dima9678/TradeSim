7c9a8fb4818cc2873d83b56d5b568fdf36ba13f7
Dima Mukharski <dmitry9678@gmail.com>
2025-01-12 23:00:59 +0300

    Добавлена функция просмотра купленных акций и функция покупки акций

--- Functions.py
+++ Functions.py
@@ -2,14 +2,19 @@ import os
 import time
 
 from Companys import companyList
+from GlobalVariables import money
 
 def start():
     os.system('cls' if os.name == 'nt' else 'clear')
 
     print("Добро пожаловать в симулятор трейдера")
-    time.sleep(2)
+    time.sleep(0.5)
     
     os.system('cls' if os.name == 'nt' else 'clear')
+
+
+
+
 
 
 def checkList():
@@ -22,3 +27,110 @@ def checkList():
     print("0. Назад")
     input()
     os.system('cls' if os.name == 'nt' else 'clear')
+
+
+
+
+
+def purchasedSharesCheck():
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+    bought = False
+
+    for i in companyList:
+        if i['purchased'] > 0:
+            bought = True
+            print(f"{i['name']:<35}Количество акций:{i['purchased']:>10.2f}")
+            time.sleep(0.02)
+
+    #Если у пользователя нет купленных акций
+    if bought == False:
+        print("У вас нет купленных акций")
+
+    print("")
+    print("0. Назад")
+    input()
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+
+
+
+def sharesBuying():
+    global money
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Список акций:")
+    j = 1
+    for i in companyList:
+        print(f"{j}. {i['name']:<35}Стоимость акции:{i['price']:>10.2f}")
+        j += 1
+        time.sleep(0.01)
+    print("")
+    print("Введите номер акции для покупки или 0 для отмены")
+
+    isEnd = False
+    answer = input()
+    answer = int(answer)
+    
+
+    if answer != 0:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Компания: " + companyList[answer - 1]["name"])
+        print("Стоимость одной акции: " + str(companyList[answer - 1]["price"]))
+        print("Акций у вас на руках: " + str(companyList[answer - 1]["purchased"]))
+        print("")
+        print("Ваш баланс: " + str(money))
+        print("")
+        print("Введите количество акций, которое хотите купить или *отмена* для отмены покупки")
+        print("")
+
+        buyAnswer = input()
+        
+
+        if buyAnswer == "отмена":
+            isEnd = True
+            os.system('cls' if os.name == 'nt' else 'clear')
+        else:
+            buyAnswer = int(buyAnswer)
+            print("Подтвердите покупку " + str(buyAnswer) + " акций на сумму " + str(buyAnswer * companyList[answer - 1]["price"]))
+            print("*да* для подтверждения, *нет* для отмены")
+
+            confirmationAnswer = input() 
+            if confirmationAnswer == "нет":
+                isEnd = True
+                os.system('cls' if os.name == 'nt' else 'clear')
+            elif confirmationAnswer == "да":
+                if money >= buyAnswer * companyList[answer - 1]["price"]:
+                    companyList[answer - 1]["purchased"] += buyAnswer
+                    money -= buyAnswer * companyList[answer - 1]["price"]
+            else:
+                print("Некорректный ввод")
+                time.sleep(1)
+                isEnd = True
+                os.system('cls' if os.name == 'nt' else 'clear')
+
+
+
+
+
+
+
+
+
+
+
+
+
+    else:
+        isEnd = True
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+
+    if isEnd == False:
+        print("0. Назад")
+        input()
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+sharesBuying()
+    
--- Main.py
+++ Main.py
@@ -3,16 +3,14 @@ import os
 import time
 import random
 
-
 from Companys import companyList
-from Functions import start, checkList
+from Functions import start, checkList, purchasedSharesCheck, sharesBuying
+from GlobalVariables import money
 
 
 start()
 
 
-
-money = 2000
 while True:
     print("Ваши деньги: " + str(money))
     print("")
@@ -24,21 +22,24 @@ while True:
     print("4. Продать акции")
     print("")
     print("5. Продолжить игру")
+    print("")
 
 
     answer = input()
     if answer == "1":
         checkList()
     elif answer == "2":
-        pass
+        purchasedSharesCheck()
     elif answer == "3":
-        pass
+        sharesBuying()
     elif answer == "4":
        pass
     elif answer == "5":
         pass
     else:
-        print("Некорректный ввод")
+        print("Некорректный ввод, попробуйте заново")
+        time.sleep(1)
+        os.system('cls' if os.name == 'nt' else 'clear')
 
 
 
